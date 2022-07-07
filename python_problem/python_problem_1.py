132c65a60177751d4c9f496e5a6fd836867fa1e4
nicholas1226 <dvc981226@gmail.com>
2022-07-08 08:47:17 +0900

    python_problem_1.6

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,29 +1,33 @@
 num = 0
-while True:
-    a = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
-    if a.isnumeric() != True:
-        print('정수를 입력하세요')
-        continue
-    elif int(a) > 3 or int(a) < 0:
-        print('1, 2, 3 중 하나를 입력하세요')
-        continue
+while (True):
+    if num < 31:
+        a = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
+        if a.isnumeric() != True:
+            print('정수를 입력하세요')
+            continue
+        elif int(a) > 3 or int(a) < 0:
+            print('1, 2, 3 중 하나를 입력하세요')
+            continue
+        else:
+            number = int(a)
+            for i in range(number):
+                print(f'playerA: {i + 1 + num}')
+            num += number
     else:
-        number = int(a)
-        for i in range(number):
-            print(f'playerA: {i + 1}')
-        num += number
         break
 
-while True:
-    a = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
-    if a.isnumeric() != True:
-        print('정수를 입력하세요')
-        continue
-    elif int(a) > 3 or int(a) < 0:
-        print('1, 2, 3 중 하나를 입력하세요')
-        continue
+    if num < 31:
+        b = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
+        if b.isnumeric() != True:
+            print('정수를 입력하세요')
+            continue
+        elif int(b) > 3 or int(b) < 0:
+            print('1, 2, 3 중 하나를 입력하세요')
+            continue
+        else:
+            number = int(b)
+            for i in range(number):
+                print(f'playerB: {i + 1 + num}')
+            num += number
     else:
-        number = int(a)
-        for i in range(number):
-            print(f'playerB: {i + 1 + num}')
         break
