d82c7d2206f2327d1464fe7018ec8720b4a3f45f
nicholas1226 <77959605+nicholas1226@users.noreply.github.com>
2022-07-08 10:21:04 +0900

    python_problem_1.8(edit)

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -2,18 +2,19 @@ def brGame():
     num = 0
     while (True):
         if num < 31:
-            a = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
-            if a.isnumeric() != True:
-                print('정수를 입력하세요')
-                continue
-            elif int(a) > 3 or int(a) < 0:
-                print('1, 2, 3 중 하나를 입력하세요')
-                continue
-            else:
-                number = int(a)
-                for i in range(number):
-                    print(f'player: {i + 1 + num}')
-                num += number
+            while(True):
+                a = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
+                if a.isnumeric() != True:
+                    print('정수를 입력하세요')
+                    continue
+                elif int(a) > 3 or int(a) < 0:
+                    print('1, 2, 3 중 하나를 입력하세요')
+                    continue
+                else:
+                    number = int(a)
+                    for i in range(number):
+                        print(f'player: {i + 1 + num}')
+                    num += number
         else:
             print('player win!')
             break
